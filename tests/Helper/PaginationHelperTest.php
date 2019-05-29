ddb058665717928bbdb16af0b9fd7ecc3ae04783
Sascha Presnac <sascha.presnac@hausperfekt.de>
2019-05-29 14:30:42 +0200

    - testing

---
+++ tests/Helper/PaginationHelperTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace spresnac\Helper\tests;
+
+use PHPUnit\Framework\TestCase;
+use spresnac\Helper\PaginationHelper;
+
+/**
+ * Class PaginationHelperTest
+ *
+ * @package Tests\Helper
+ */
+
+class PaginationHelperTest extends TestCase
+{
+    /** @test */
+    public function generate_pagination_with_array(): void
+    {
+         $items = ['one' => 1, 'two' => 2];
+         $result = PaginationHelper::paginate_collection($items);
+         $this->assertTrue(is_array($result));
+         $this->assertArrayHasKey('data', $result);
+         $this->assertArrayHasKey('links', $result);
+         $this->assertArrayHasKey('meta', $result);
+        $this->assertCount(2, $result['data']);
+        $this->assertCount(4, $result['links']);
+        $this->assertCount(6, $result['meta']);
+    }
+    /** @test */
+    public function generate_pagination_with_collection(): void
+    {
+         $items = collect(['one' => 1, 'two' => 2]);
+         $result = PaginationHelper::paginate_collection($items);
+         $this->assertTrue(is_array($result));
+         $this->assertArrayHasKey('data', $result);
+         $this->assertArrayHasKey('links', $result);
+         $this->assertArrayHasKey('meta', $result);
+        $this->assertCount(2, $result['data']);
+        $this->assertCount(4, $result['links']);
+        $this->assertCount(6, $result['meta']);
+    }
+}
